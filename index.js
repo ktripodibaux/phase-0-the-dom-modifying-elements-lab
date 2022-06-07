f36acad726b01b809c7550332b9e271f15b13001
Kurt Baux <ktripodibaux@gmail.com>
2022-06-07 16:51:02 -0400

    commit tree 61ade36d340a2e67af807bce8f133e648a8e751e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// const main = document.createElement('div');
+// main.id= 'main';
+
+// document.body.append('main');
+const main = document.getElementById('main');
+main.remove();
+document.getElementsByClassName
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+
+
+// newHeader.innerText = 'YOUR-NAME is the champion';
+// newHeader.innerHTML = 'YOUR-NAME is the champion';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.append(newHeader);
